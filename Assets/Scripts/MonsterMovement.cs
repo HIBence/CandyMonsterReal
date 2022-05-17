27aa5f4be9edfb9d886a42fa752b56d434d387f1
Bence <bence.ch@gmail.com>
2022-05-17 18:35:28 +0200

    Demo

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] targets; 
+
+    private NavMeshAgent Monster;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Monster = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveOverPathOne()
+    {
+        Monster.SetDestination(targets[0].transform.position);
+    }
+}
